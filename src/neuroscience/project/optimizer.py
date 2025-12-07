eb47aef95c833f4b353bd3a2608b1730a1d24baf
IgnorantCopy <231880174@smail.nju.edu.cn>
2025-12-08 01:51:39 +0800

    <temp>

--- src/neuroscience/project/optimizer.py
+++ src/neuroscience/project/optimizer.py
@@ -2,48 +2,6 @@ from neuron import *
 from typing import List, Callable
 
 
-# class STDP:
-#     def __init__(self, synapse_groups: List[SynapseGroup],
-#                  tau_pre: float = 2., tau_post: float = 2.,
-#                  f_pre: Callable = lambda x: x, f_post: Callable = lambda x: x):
-#         self.synapse_groups = synapse_groups
-#         self.tau_pre = tau_pre
-#         self.tau_post = tau_post
-#         self.f_pre = f_pre
-#         self.f_post = f_post
-#         self.trace_pre = [np.zeros(len(synapse_group.pre_group.neurons)) for synapse_group in self.synapse_groups]
-#         self.trace_post = [np.zeros(len(synapse_group.post_group.neurons)) for synapse_group in self.synapse_groups]
-#         self.delta_w = [np.zeros((len(synapse_group.pre_group.neurons), len(synapse_group.post_group.neurons)))
-#                         for synapse_group in self.synapse_groups]
-#
-#     def step(self):
-#         for i, synapse_group in enumerate(self.synapse_groups):
-#             n_in = len(synapse_group.pre_group.neurons)
-#             n_out = len(synapse_group.post_group.neurons)
-#             weight = np.array([synapse.weight for synapse in synapse_group.synapses]).reshape((n_in, n_out))
-#             spike_pre = np.array([float(neuron.is_spike()) for neuron in synapse_group.pre_group.neurons])
-#             spike_post = np.array([float(neuron.is_spike()) for neuron in synapse_group.post_group.neurons])
-#
-#             self.trace_pre[i] = self.trace_pre[i] - self.trace_pre[i] / self.tau_pre + spike_pre
-#             self.trace_post[i] = self.trace_post[i] - self.trace_post[i] / self.tau_post + spike_post
-#             delta_w_pre = -self.f_pre(weight) * (self.trace_post[i][None, :] * spike_pre[:, None])
-#             delta_w_post = self.f_post(weight) * (self.trace_pre[i][:, None] * spike_post[None, :])
-#             self.delta_w[i] += delta_w_pre + delta_w_post
-#
-#     def reset(self):
-#         self.trace_pre = [np.zeros(len(synapse_group.pre_group.neurons)) for synapse_group in self.synapse_groups]
-#         self.trace_post = [np.zeros(len(synapse_group.post_group.neurons)) for synapse_group in self.synapse_groups]
-#         self.delta_w = [np.zeros((len(synapse_group.pre_group.neurons), len(synapse_group.post_group.neurons)))
-#                         for synapse_group in self.synapse_groups]
-#
-#     def apply(self):
-#         for i, synapse_group in enumerate(self.synapse_groups):
-#             n_out = len(synapse_group.post_group.neurons)
-#             for j, synapse in enumerate(synapse_group.synapses):
-#                 synapse.weight -= self.delta_w[i][j // n_out, j % n_out]
-#         self.reset()
-
-
 class STDP(Node):
     def __init__(self, synapse_groups: List[SynapseGroup],
                  tau_pos: float = 10., tau_neg: float = 10.,
@@ -107,3 +65,5 @@ class STDP(Node):
                             delta_t = t_post - t_pre
                             dw += self.w_fn(synapse.weight, delta_t)
                     synapse.weight -= dw
+                    synapse.weight = np.clip(synapse.weight, -1.5, 1.5)
+        self.reset()
